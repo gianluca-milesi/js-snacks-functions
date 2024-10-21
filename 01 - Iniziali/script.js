3c2e5f65f93b863327d91c3cf42cb3991dab1a81
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-21 16:09:08 +0200

    ADD: first snack, initials of an array

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initialsArray(namesArray) {
+    let initialsName = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        initialsName.push(namesArray[i][0]);
+    }
+    return initialsName;
+}
+
+
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const initials = initialsArray(names);
+console.log(initials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
