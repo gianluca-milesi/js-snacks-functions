6b97da8f9acdd2ec839315763ffe7e6f2c066ce1
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-21 17:06:00 +0200

    ADD: third snack, vowels count

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,20 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+function countVowels(string) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let counter = 0;
 
+    for (let i = 0; i < string.lenght; i++) {
+        if (vowels.includes(string[i].toLowerCase())) {
+            counter = counter + 1;
+        }
+    }
+    return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+const stringVowels = countVowels(word);
+console.log(stringVowels);
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
